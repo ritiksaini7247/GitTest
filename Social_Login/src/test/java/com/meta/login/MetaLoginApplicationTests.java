5b6d46e2aee7caf9d442c8d4ebedd0e7efa617bb
Ritik <56201149+ritiksaini7247@users.noreply.github.com>
2022-05-17 12:39:16 +0530

    Add files via upload
    Social Login Commit 1

---
+++ Social_Login/src/test/java/com/meta/login/MetaLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
